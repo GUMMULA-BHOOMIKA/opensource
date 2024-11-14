bfb0aa6912b2c5cf4229b6d3d7963c0765713354
GUMMULA-BHOOMIKA <gummulabhoomika2003@gmail.com>
2024-11-14 14:45:33 +0530

    Create SeasonGame.py

---
+++ vissa_prep/SeasonGame.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n<=12:
+    if n==3 or n==4 or n==5:
+        print("Spring")
+    if n==6 or n==7 or n==8:
+        print("Summer")
+    if n==9 or n==10 or n==11:
+        print("Autumn")
+    if n==12 or n==1 or n==2:
+        print("Winter")
+    
+else:
+    print("Invalid")
